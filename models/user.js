0794c7843a6c16fcaee7483f9ade0e2aa31158cd
desperadoray <rza21.ca@gmail.com>
2017-03-27 00:39:42 -0700

    add /authenticate POST for API token

--- models/user.js
+++ models/user.js
@@ -1,43 +1,50 @@
 const mongoose = require('mongoose');
-const bcrypy = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const config = require('../config/database');
 
-//user schema
+// User Schema
 const UserSchema = mongoose.Schema({
-	name: {
-		type: String
-	},
-	email: {
-		type: String,
-		required: true
-	},
-	username: {
-		type : String,
-		required : true
-	},
-	password: {
-		type: String,
-		required : true
-	}
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 const User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.getUserById = function(id, callback){
-	User.findById(id, callback);
+  User.findById(id, callback);
 }
 
-module.exports.getUserByUsername = function(id, callback){
-	const query = {username: username}
-	User.findOne(query, callback);
+module.exports.getUserByUsername = function(username, callback){
+  const query = {username: username}
+  User.findOne(query, callback);
 }
 
 module.exports.addUser = function(newUser, callback){
-	bcrypy.genSalt(10, (err, salt) => {
-		bcrypy.hash(newUser.password, salt, (err,hash) =>{
-			if(err) throw err;
-			newUser.password = hash;
-			newUser.save(callback);
-		});
-	});
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if(err) throw err;
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+    if(err) throw err;
+    callback(null, isMatch);
+  });
 }
